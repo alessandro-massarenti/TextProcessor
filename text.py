fc985e2bcdbbcc6459911f562593dacd63501e40
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2025-03-08 19:56:14 +0100

    added "chunked text" property to class Text

--- text.py
+++ text.py
@@ -5,7 +5,7 @@ class Text:
 
     def __init__(self, text: str, text_processor: TextPreprocessor):
         self.__text_preprocessor = text_processor
-        self.__text: list = self.__text_preprocessor.chunked_text(text)
+        self.__text: list[str] = self.__text_preprocessor.chunked_text(text)
 
     def change_text_preprocessor(self, text_processor: TextPreprocessor):
         self.__text_preprocessor = text_processor
@@ -18,8 +18,14 @@ class Text:
         translated_text = self.__text_preprocessor.replaced_text(full_text)
         self.__text = self.__text_preprocessor.chunked_text(translated_text)
 
+    @property
+    def chunked_text(self) -> list[str]:
+        return self.__text
+
+
     def __str__(self) -> str:
         return " ".join(self.__text)
+
 
 
 
